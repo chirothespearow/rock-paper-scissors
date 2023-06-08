c0086ce51308abe70c0c239daff5ffaf4d1db40b
chirothespearow <ritvikrrao@gmail.com>
2023-06-08 11:20:07 +0530

    Add basic UI

--- script.js
+++ script.js
@@ -10,45 +10,79 @@ function getComputerChoice(){
     }
 }
 
-function playRound(playerChoice,computerChoice){
-    if(playerChoice === computerChoice){
-        return 'Draw!';
-    }
-    else if(playerChoice === 'rock'){
-        if (computerChoice === 'paper'){
-            return 'You lose :(';
+function playRound(e){
+    if (game){
+        let playerChoice = this.classList.value;
+        let computerChoice = getComputerChoice();
+        if(playerChoice === computerChoice){
+            div.textContent = 'Draw!';
         }
-        return 'You win!'
-    }
-    else if(playerChoice === 'paper'){
-        if (computerChoice === 'scissors'){
-            return 'You lose :(';
-        }
-        return 'You win!'
-    }
-    else if(playerChoice === 'scissors'){
-        if (computerChoice === 'rock'){
-            return 'You lose :(';
-        }
-        return 'You win!'
-    }
-}
-
-function game(){
-    for(i=0;i<5;i++){
-        while(true){
-            var playerChoice = prompt("Choose your weapon: ");
-            playerChoice = playerChoice.toLowerCase();
-            if(playerChoice === 'scissors' || playerChoice === 'paper' || playerChoice === 'rock'){
-                break;
+        else if(playerChoice === 'rock'){
+            if (computerChoice === 'paper'){
+                div.textContent = 'You lose :(';
+                cScore++;
             }
-            alert("That is not a valid weapon!");
+            else{
+                div.textContent = 'You win!';
+                pScore++;
+            }
         }
+        else if(playerChoice === 'paper'){
+            if (computerChoice === 'scissors'){
+                div.textContent = 'You lose :(';
+                cScore++;
+            }
+            else{
+                div.textContent = 'You win!';
+                pScore++;
+            }
+        }
+        else if(playerChoice === 'scissors'){
+            if (computerChoice === 'rock'){
+                div.textContent = 'You lose :(';
+                cScore++;
+            }
+            else{
+                div.textContent = 'You win!';
+                pScore++;
+            }
+        }
+    }   
+    playerScore.textContent = `Player Score : ${pScore}`;
+    computerScore.textContent = `Computer Score : ${cScore}`;
 
-        console.log(playerChoice);
-        alert(playRound(playerChoice,getComputerChoice())); 
+    if(pScore === 5 || cScore === 5){
+        game = false;
+        if (pScore > cScore){
+            result.textContent = "Player wins!";
+        }
+        else{
+            result.textContent = "Computer wins!";
+        }
     }
-    alert("The game has finished.");
 }
 
-game();
+var pScore = 0;
+var cScore = 0;
+var game = true;
+const buttons = document.querySelectorAll('button');
+buttons.forEach(btn => btn.addEventListener('click',(playRound)));
+const container = document.querySelector('body');
+const div = document.createElement('div');
+container.appendChild(div);
+
+const playerScore = document.createElement('div');
+playerScore.textContent = `Player Score : ${pScore}`;
+container.appendChild(playerScore); 
+
+const computerScore = document.createElement('div');
+computerScore.textContent = `Computer Score : ${cScore}`;
+container.appendChild(computerScore); 
+
+const result = document.createElement('div');
+container.appendChild(result);
+// while(pScore<5 && cScore<5){
+
+// }
+
+// game=false;
